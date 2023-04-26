221e0f0cf7cb92dec93cd546a6e1163bcdd43d60
Marina Zonova <114925418+marinazonz@users.noreply.github.com>
2023-04-26 12:44:17 +0200

    Added design and sanity pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <div className='navbar-container'>
+            <p className='logo'>
+                <Link href='/'>Headphones Store</Link>
+            </p>
+            <button type='button' className='cart-icon' onClick=''>
+                <AiOutlineShopping />
+                <span className='cart-item-qty'>0</span>
+            </button>
+        </div>
+    );
 };
 
 export default Navbar;
